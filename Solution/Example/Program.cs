062616f1348a0c834331d2e93a198912193f0054
marcos_jujuy17 <marcos_jujuy17@FER_VAIO>
2018-05-17 15:19:36 -0300

    testing the github "thing"

---
+++ Solution/Example/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("this is an example");
+            Console.ReadKey();
+        }
+    }
+}
